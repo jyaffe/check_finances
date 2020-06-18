a3b85dd9f16c42689c27302444a9a720f18f6c67
jyaffe <44579282+jyaffe@users.noreply.github.com>
2020-06-18 13:58:43 +0100

    handle personal and joint accounts

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ class MonzoClient:
     def __init__(self):
         self._api_client = oauth2.OAuth2Client()
         self._api_client_ready = False
-        self._account_id = None
-        self._balances = []
+        self._personal_account_id = None
+        self._joint_account_id = None
+        self.balances = []
 
 
     def check_auth(self):
@@ -73,36 +74,41 @@ class MonzoClient:
 
 
     def get_account(self):
-        ''' Retreive information of the authorised user's current account information, rather than from a joint account if present. This will be changed to store joint details too in future'''
+        ''' Retreive information of the authorised user's personal and joint account information '''
 
         print('Retrieving account information...')
         success, response = self._api_client.api_get('accounts',{})
         if not success or 'accounts' not in response or len(response['accounts']) < 1:
             error('Count not retrieve accounts information: {}'.format(response))
 
-        # this restricts it to personal accounts only.
         for account in response['accounts']:
             if 'type' in account and account['type'] == 'uk_retail':
-                self._account_id = account['id']
-                print('Retrieved account information.')
-                return
+                self._personal_account_id = account['id']
+                print('Retrieved personal account information.')
+            if 'type' in account and account['type'] == 'uk_retail_joint':
+                self._joint_account_id = account['id']    
+                print('Retrieved joint account information.')
+            
         
-        if self._account_id is None:
+        if self._personal_account_id is None:
             error('Count not find a personal account')
+        
+        if self._joint_account_id is None:
+            error('Count not find a joint account')
             
 
-    def list_balances(self):
+    def list_balances(self,account):
         ''' An example call to the end point documented in https://docs.monzo.com/#balance'''
 
         if self._api_client is None or not self._api_client_ready:
             error('API client not initialised.')
 
-        success, response = self._api_client.api_get('balance',{'account_id': self._account_id})
+        success, response = self._api_client.api_get('balance',{'account_id': account})
 
         if not success or 'balance' not in response:
             error('Could not get balance ({})'.format(response))
 
-        self._balances = response['balance']
+        self.balances = response['balance']
         print('Balance loaded.')
 
 
@@ -110,5 +116,5 @@ if __name__ == '__main__':
     monzo =  MonzoClient()
     monzo.check_auth()
     monzo.get_account()
-    monzo.list_balances()
-    print(monzo._balances)
+    monzo.list_balances(monzo._personal_account_id)
+    print(monzo.balances)
