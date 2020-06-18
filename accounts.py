7e35c727dc0904164f11418fd85852c791e0ce53
jyaffe <44579282+jyaffe@users.noreply.github.com>
2020-06-18 16:14:07 +0100

    account and pot classes

---
+++ accounts.py
@@ -0,0 +1,43 @@
+from utils import error
+
+class Account:
+    ''' Takes the response object from the get accounts API call and creates an account object based on the retuned details '''
+
+    def __init__(self,account):
+        self.id = account['id']
+        self.description = account['description']
+
+        if 'type' in account and account['type'] == 'uk_retail':
+            self.type = 'personal'
+            print('Retreived personal account information')
+        elif 'type' in account and account['type'] == 'uk_retail_joint':
+            self.type = 'joint'
+            print('Retreived joint account information')
+        else:
+            error('Account type not recognised')
+    
+    def add_main_balance(self,response):
+        self.main_balance = response['balance']/100
+        self.total_balance = response['total_balance']/100
+
+    def add_pot_balances(self,response):
+        self.pots = []
+
+        for pot in response['pots']:
+            self.pots.append(Pot(pot))
+
+    def __repr__(self):
+        return '{} {}'.format(self.description,self.type)
+        
+
+class Pot:
+    ''' Creates a pot object from the pots get API call '''
+
+    def __init__(self,pot):
+        self.id = pot['id']
+        self.name = pot['name']
+        self.balance = pot['balance']/100
+        self.deleted = pot['deleted']
+
+    def __repr__(self):
+        return '{}'.format(self.name)
